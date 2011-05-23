699d552064e186a161d7927d5a6cf937947a7132
David Precious <davidp@preshweb.co.uk>
2011-05-23 12:03:09 +0100

    Muppet.

--- lib/Dancer/Plugin/TimeRequests.pm
+++ lib/Dancer/Plugin/TimeRequests.pm
@@ -39,7 +39,7 @@ before sub {
 after sub {
     Dancer::Logger::debug(sprintf "Request to %s completed in %2.f",
         request->path,
-        vars->{request_start_time} - Time::HiRes::time()
+        Time::HiRes::time() - vars->{request_start_time};
     );
 };
 
