dac7941a4c61d69942778bcd252914457f4c8c96
David Precious <davidp@preshweb.co.uk>
2012-11-21 18:14:32 +0000

    Add per-route time recording & reporting.
    After each route execution, record how long it took.
    
    Add report page to produce a list of routes along with average execution time,
    so we can see which routes are slowest.
    
    TODO: don't keep an unlimited number of times - on a busy site, it'll eat up
    more and more RAM.
    
    TODO: maybe more detailed reporting, perhaps overhauling to record timestamp of
    each record so it could produce a graph.

--- lib/Dancer/Plugin/TimeRequests.pm
+++ lib/Dancer/Plugin/TimeRequests.pm
@@ -3,21 +3,26 @@ package Dancer::Plugin::TimeRequests;
 use strict;
 use Dancer::Plugin;
 use Dancer qw(:syntax);
+use HTML::Table;
+use List::Util;
+use List::MoreUtils;
 use Time::HiRes;
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 =head1 NAME
 
-Dancer::Plugin::TimeRequests - log how long each request took to process
+Dancer::Plugin::TimeRequests - log how long requests take and which routes are slow
 
 =head1 DESCRIPTION
 
-A simple Dancer plugin to log how long each request took to process.  Useful to
-examine performance.
+A simple Dancer plugin to log how long each request took to process, and also to
+gather stats on the average response time for each route - so you can see at a
+glance which routes are taking longer than you'd like, therefore where you ought
+to start looking to improve performance.
 
-Future versions will likely add a stats page to show which routes take most
-time, etc.
+Provides a statistics page giving you a list of your routes, along with their
+response times.
 
 
 =head1 SYNOPSIS
@@ -31,9 +36,15 @@ showing how logn each request took:
 
     Request to /foo completed in 4.0011 seconds in ....
 
+To see which routes are slow, hit the URL C</plugin-timerequests>. 
+
 =cut
 
+my %request_times;
+
 hook before => sub {
+    my $route_handler = shift;
+    var current_handler => $route_handler;
     var request_start_time => Time::HiRes::time();
 };
 
@@ -42,7 +53,82 @@ hook after => sub {
         request->path,
         Time::HiRes::time() - vars->{request_start_time}
     );
+    push @{ $request_times{ vars->{current_handler} } }, 
+        Time::HiRes::time() - vars->{request_start_time};
 };
+
+get '/plugin-timerequests' => sub {
+    # Get the list of routes, and for each one, match up the coderef with our
+    # recorded times, and add the timing info, so we can then sort routes by
+    # average execution time to produce the output
+    my $routes = _get_routes();
+    for my $route (@$routes) {
+        my $route_times = $request_times{ $route->{route} };
+        next unless defined $route_times && scalar @$route_times;
+
+        my ($min, $max) = List::MoreUtils::minmax(@$route_times); 
+        $route->{times} = {
+            avg => List::Util::sum(@$route_times) / @$route_times,
+            min => $min,
+            max => $max,
+        };
+    }
+
+    # Now, we can loop through all routes, slowest first, and output the timing
+    # info
+    my $table = HTML::Table->new;
+    $table->addRow('Route pattern', 'Average', 'Best', 'Worst');
+    $table->setRowHead(1);
+    for my $route (
+        sort { $b->{times}{avg} <=> $a->{times}{avg} }
+        grep { exists $_->{times} } @$routes
+    ) {
+        next unless exists $route->{times};
+        my $times = $route->{times};
+        $table->addRow(
+            $route->{pattern},
+            map { sprintf '%.3f', $_ || 0 } @$times{qw(avg min max)},
+        );
+    }
+    return $table->getTable;
+
+};
+
+# Fetch all routes defined.  (Loosely based on code lovingly stolen with no
+# shame from Dancer::Plugin::SiteMap - cheers James Ronan (JNRONAN)
+# Returns an arrayref of hashrefs describing all routes (with keys pattern 
+# and handler)
+sub _get_routes {
+    my $version = (exists &dancer_version) ? int( dancer_version() ) : 1;
+    my @apps    = ($version == 2) ? @{ runner->server->apps }
+                                  : Dancer::App->applications;
+ 
+    my @routes;
+    for my $app ( @apps ) {
+        my $app_routes = ($version == 2) ? $app->routes
+                                         : $app->{registry}->{routes};
+        
+        for my $route_type (keys %$app_routes) {
+            for my $route (@{ $app_routes->{$route_type} }) {
+                my ($pattern, $handler);
+                if ($version == 2) {
+                    $pattern = $route->spec_route;
+                    $handler = $route->handler;
+                } else {
+                    $pattern = $route->pattern;
+                    $handler = $route->code;
+                }
+                push @routes, {
+                    route   => $route,
+                    pattern => $pattern, 
+                    handler => $handler,
+                };
+            }
+        }
+    }
+    debug "list of routes being returned:", \@routes;
+    return \@routes;
+}
 
 
 =head1 AUTHOR
